d53220e4002ee40b33f03310b9b760153990a593
Piotr Matlak <piotr.matlak.contact@gmail.com>
2022-07-10 21:19:38 +0200

    add solve sudoku function.

---
+++ game.js
@@ -0,0 +1,36 @@
+let board = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+];
+
+function isValidPlace(grid, row, col, number){
+    
+}
+
+function solve(grid) {
+  for (let row = 0; row < 9; row++) {
+    for (let col = 0; col < 9; col++) {
+      if (grid[row][col] === 0) {
+        for (let guess = 1; guess < 10; guess++) {
+          if (isValidPlace(grid, row, col, guess)) {
+            grid[row][col] = guess;
+            if (solve(grid)) {
+              return true;
+            }
+            grid[row][col] = 0;
+          }
+        }
+        return false;
+      }
+    }
+  }
+}
+
+solve(board);
